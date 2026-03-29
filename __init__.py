7445702761da76c086875817734ea94bacb9dc5b
Ck-arch1 <Chetanya123456kapoor@gmail.com>
2026-03-29 11:09:41 +0530

    task-files

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from tasks.task_easy   import get_task as get_easy_task
+from tasks.task_medium import get_task as get_medium_task
+from tasks.task_hard   import get_task as get_hard_task
+
+_all_ = ["get_easy_task", "get_medium_task", "get_hard_task"]
+
+
+
+
